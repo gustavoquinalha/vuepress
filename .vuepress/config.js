a772dc891b5cf8a8fb856e726dc036ef19c2fae3
gustavoquinalha <gustavo_quinalha@hotmail.com>
2018-09-28 16:31:24 +0000

    Create template :boom: :rocket:

---
+++ .vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    title: 'Vuepress Blog',
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Blog', link: '/blog/' }
+        ],
+        sidebar: [
+            '/',
+            '/blog/'
+        ]
+    }
+}
